e0c1c4beee0c41cdbbcfda624c1783e27702fb85
Devin Bell <123232883+MajorBreach@users.noreply.github.com>
2023-04-27 07:10:26 -0400

    Delete Python w~MySQL directory

--- Python w~MySQL/Authors/Project/Server/routes/form.routes.js
+++
@@ -1,9 +0,0 @@
-const Controller = require('../controllers/form.controller');
-
-module.exports = (app) =>{
-    app.get('/api/author', Controller.findAll);
-    app.post('/api/author', Controller.create);
-    app.get('/api/author/:id', Controller.findOne);
-    app.put('/api/author/:id', Controller.update);
-    app.delete('/api/author/:id', Controller.delete);
-};
